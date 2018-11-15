b6c189b5b91b726804d49c5321818488eef82aaf
Choiyou <os1502@naver.com>
2018-11-15 14:01:00 +0900

    프로필 레이아웃 수정 / 비밀번호 변경

--- app/src/main/java/com/example/os150/tabhost/ProfileActivity.java
+++ app/src/main/java/com/example/os150/tabhost/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.os150.tabhost;
 
+        import android.app.Dialog;
         import android.content.DialogInterface;
         import android.content.Intent;
         import android.os.Bundle;
@@ -7,8 +8,10 @@ package com.example.os150.tabhost;
         import android.support.annotation.Nullable;
         import android.support.v7.app.AlertDialog;
         import android.support.v7.app.AppCompatActivity;
+        import android.util.Log;
         import android.view.View;
         import android.widget.Button;
+        import android.widget.EditText;
         import android.widget.ImageView;
         import android.widget.TextView;
         import android.widget.Toast;
@@ -32,11 +35,13 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
     //view objects
     private TextView textViewUserEmail;
-    private TextView Using;
-    private TextView Username;
+    private Button Using;
+    private Button ChangePassword;
+    private EditText Username;
     private ImageView imageView;
     private Button buttonLogout;
-    private TextView userDelete;
+    private Button userDelete;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,10 +51,12 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         //initializing views
         textViewUserEmail = (TextView) findViewById(R.id.textviewUserEmail);
         buttonLogout = (Button) findViewById(R.id.btnLogout);
-        userDelete = (TextView) findViewById(R.id.userDelete);
-        Username = (TextView) findViewById(R.id.username);
-        Using = (TextView) findViewById(R.id.Using);
+        userDelete = (Button) findViewById(R.id.userDelete);
+        Username = (EditText) findViewById(R.id.username);
+        Using = (Button) findViewById(R.id.Using);
+        ChangePassword =(Button)findViewById(R.id.btnChangePassword);
         firebaseAuth = FirebaseAuth.getInstance();
+
         if(firebaseAuth.getCurrentUser() == null) {
             finish();
             startActivity(new Intent(this, LoginActivity.class));
@@ -62,7 +69,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             String photoUrl = user.getPhotoUrl().toString();
             ImageView photoImageView = (ImageView) findViewById(R.id.profileimage);
             Glide.with(this).load(photoUrl).into(photoImageView);
-
+            
         }
 
         //textViewUserEmail의 내용을 변경해 준다.
@@ -72,6 +79,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         Using.setOnClickListener(this);
         buttonLogout.setOnClickListener(this);
         userDelete.setOnClickListener(this);
+        ChangePassword.setOnClickListener(this);
 
 
     }
@@ -87,13 +95,44 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             finish();
             startActivity(new Intent(this, MainActivity.class));
         }
-        //회원탈퇴를 클릭하면 회원정보를 삭제한다.
-        if(view == userDelete) {
-            AlertDialog.Builder alert_confirm = new AlertDialog.Builder(ProfileActivity.this);
-            alert_confirm.setMessage("정말 계정을 삭제 할까요?").setCancelable(false).setPositiveButton("확인", new DialogInterface.OnClickListener() {
+        if(view == ChangePassword){
+            final EditText editCPassword = new EditText(ProfileActivity.this);
+            editCPassword.getTransformationMethod();
+            AlertDialog.Builder alert_confirm1 = new AlertDialog.Builder(ProfileActivity.this);
+            alert_confirm1.setTitle("비밀번호 변경");
+            alert_confirm1.setMessage("새 Password 입력");
+            alert_confirm1.setView(editCPassword);
+            alert_confirm1.setMessage("비밀번호 변경 이후 되돌릴 수 없습니다.").setCancelable(false).setPositiveButton("확인", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
-                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            user.updatePassword(editCPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            Toast.makeText(ProfileActivity.this, "비밀번호가 변경 되었습니다.", Toast.LENGTH_LONG).show();
+                                            finish();
+                                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                        }
+                                    });
+                        }
+                    }
+            );
+            alert_confirm1.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(ProfileActivity.this, "취소", Toast.LENGTH_LONG).show();
+                }
+            });
+            alert_confirm1.show();
+//            finish();
+//            startActivity(new Intent(this,FindActivity.class));
+            }
+
+        //회원탈퇴를 클릭하면 회원정보를 삭제한다.
+        if(view == userDelete) {
+            AlertDialog.Builder alert_confirm2 = new AlertDialog.Builder(ProfileActivity.this);
+            alert_confirm2.setMessage("정말 계정을 삭제 할까요?").setCancelable(false).setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
                             user.delete()
                                     .addOnCompleteListener(new OnCompleteListener<Void>() {
                                         @Override
@@ -106,13 +145,13 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
                         }
                     }
             );
-            alert_confirm.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            alert_confirm2.setNegativeButton("취소", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     Toast.makeText(ProfileActivity.this, "취소", Toast.LENGTH_LONG).show();
                 }
             });
-            alert_confirm.show();
+            alert_confirm2.show();
         }
     }
 }
